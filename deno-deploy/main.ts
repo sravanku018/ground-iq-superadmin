5d0e40f3928150eb263b1f781346dd7660ed8ca3
sravanku018 <sravanku018@gmail.com>
2026-08-28 20:52:13 +0530

    Bump version to v2.0.21 (versionCode 20021)

--- deno-deploy/main.ts
+++ deno-deploy/main.ts
@@ -4201,8 +4201,8 @@ async function rawHandler(req: Request): Promise<Response> {
       return json(
         {
           appName: "Smart Survey X",
-          version: "2.0.20",
-          versionCode: 20020,
+          version: "2.0.21",
+          versionCode: 20021,
           minSupportedVersionCode: 20000,
           apkUrl: `https://github.com/${repo}/releases/latest/download/ElectionSurvey-release.apk`,
           apkDebugUrl: `https://github.com/${repo}/releases/latest/download/ElectionSurvey-debug.apk`,
